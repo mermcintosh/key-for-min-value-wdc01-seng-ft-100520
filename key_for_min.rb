da6ddf9d86a5bb426e30c8d41d37fe4046e382d7
Meredith McIntosh <meredith@mcintosh.io>
2020-09-24 13:38:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,36 +7,11 @@ require "pry"
 #assign lowest_value before the loop to be the value I just retrieved.
 
 def key_for_min_value(hash)
-  hash.each do |key, value|
-end key_for_min_value({:shirt => 15, :skirt => 30, :dress => 40, :shoes => 5, :jeans => 45})
 
-# def key_for_min_value(hash)
-# current_hash_pair = [0]
-# hash_w_lowest_value = hash.first
-#   while current_hash_pair < hash.count do
-    
-#     if hash_w_lowest_value < hash[curent_hash_pair]
-#       hash_w_lowest_value << hash[curent_hash_pair]
-#     end
-#     current_index += 1
-#   end
-#   lowest_value.fetch(key)
-# end
+end 
 # key_for_min_value({:shirt => 15, :skirt => 30, :dress => 40, :shoes => 5, :jeans => 45})
 
-# def key_for_min_value(hash)
-# current_index = 0
-# hash_w_lowest_value = hash.first
-#   while current_index < hash.count do
-#     if hash_w_lowest_value.value < hash[curent_index].value
-#       hash_w_lowest_value = hash[curent_index]
-#     end
-#     binding.pry
-#     current_index += 1
-#   end
-#   lowest_value.fetch(key)
-# end
-# key_for_min_value({:shirt => 15, :skirt => 30, :dress => 40, :shoes => 5, :jeans => 45})
+
 
 
 
